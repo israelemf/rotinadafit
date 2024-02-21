ee8489f1b65aedc919965d6e955b260af45d7f23
Israel Meloso Filho <israelemf@outlook.com>
2024-02-20 23:13:36 -0300

    Add Springboot project

---
+++ backend/src/main/java/com/bytematrix/rotinadafit/RotinadafitApplication.java
@@ -0,0 +1,13 @@
+package com.bytematrix.rotinadafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotinadafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotinadafitApplication.class, args);
+	}
+
+}
