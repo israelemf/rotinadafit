b64540b6482a943ce132fadc71172c0f7db100b6
Israel Meloso Filho <israelemf@outlook.com>
2024-02-21 00:00:39 -0300

    Added initial Entity for User

---
+++ backend/src/main/java/com/bytematrix/rotinadafit/entities/User.java
@@ -0,0 +1,36 @@
+package com.bytematrix.rotinadafit.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String phone;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public UUID getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
