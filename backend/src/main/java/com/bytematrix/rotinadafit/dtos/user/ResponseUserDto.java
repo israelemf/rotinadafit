f15987c22e0643ce942deb818812c9ead0aa96d1
Israel Meloso Filho <israelemf@outlook.com>
2024-02-22 00:39:40 -0300

    Ajustes DTOs

--- backend/src/main/java/com/bytematrix/rotinadafit/dtos/user/ResponseUserDto.java
+++ backend/src/main/java/com/bytematrix/rotinadafit/dtos/user/ResponseUserDto.java
@@ -3,8 +3,10 @@ package com.bytematrix.rotinadafit.dtos.user;
 import jakarta.validation.constraints.Email;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record ResponseUserDto(String username,
+public record ResponseUserDto(UUID id,
+                              String username,
                               String email,
                               String phone,
                               LocalDateTime createdAt) {
--- backend/src/main/java/com/bytematrix/rotinadafit/dtos/user/UpdateUserDto.java
+++ backend/src/main/java/com/bytematrix/rotinadafit/dtos/user/UpdateUserDto.java
@@ -1,11 +1,14 @@
 package com.bytematrix.rotinadafit.dtos.user;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.UUID;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record UpdateUserDto(UUID id,
                             String username,
-                            @Email
                             String email,
                             String password,
                             String phone) {
