b77a871dce95d77c96eb830153fd7ee28e95fac5
Israel Meloso Filho <israelemf@outlook.com>
2024-03-04 23:39:45 -0300

    Added initial attribute validation messages

--- backend/src/main/java/com/bytematrix/rotinadafit/dtos/user/CreateUserDto.java
+++ backend/src/main/java/com/bytematrix/rotinadafit/dtos/user/CreateUserDto.java
@@ -2,13 +2,16 @@ package com.bytematrix.rotinadafit.dtos.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record CreateUserDto(@NotBlank
                             String username,
-                            @NotBlank @Email
+                            @NotBlank @Email(message = "Informe um email válido!")
                             String email,
                             @NotBlank
+                            @Size(min = 8, message = "A senha exige no minimo 8 caracteres!")
                             String password,
                             @NotBlank
+                            @Size(min = 11, max = 11, message = "O número de telefone deve conter 11 digitos!")
                             String phone) {
 }
